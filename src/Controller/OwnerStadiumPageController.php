9d9aea6c927ce6e8a031349f35177458cfc15e47
Amir-Mallek <amir.mallek@insat.ucar.tn>
2024-04-22 18:03:40 +0100

    add stadium basic front

---
+++ src/Controller/OwnerStadiumPageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class OwnerStadiumPageController extends AbstractController
+{
+    #[Route('/ownerStadiumPage', name: 'app_owner_stadium_page')]
+    public function index(): Response
+    {
+        return $this->render('owner_stadium_page/index.html.twig', [
+            'amir' => 50,
+        ]);
+    }
+}
