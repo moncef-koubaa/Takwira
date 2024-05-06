c3d1dab8eda92ce83ef30223a8af1aed01b26668
MouhibBahri <mouhib.bahri@insat.ucar.tn>
2024-05-06 15:20:16 +0100

    test

--- src/Controller/OwnerStadiumPageController.php
+++ src/Controller/OwnerStadiumPageController.php
@@ -11,6 +11,7 @@ class OwnerStadiumPageController extends AbstractController
     #[Route('/ownerStadiumPage', name: 'app_owner_stadium_page')]
     public function index(): Response
     {
+        //test test
         return $this->render('owner_stadium_page/index.html.twig', [
             'amir' => 50,
         ]);
